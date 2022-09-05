d48495dcbd339dd36be4587a3352b3cfc8e9a812
DanilaCrazych <Mag7istr@mail.ru>
2022-09-05 22:18:29 +0300

    Добавьте файлы проекта.

---
+++ Slave/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Mat
+{
+
+    internal class Program
+    {
+
+        private static void Main()
+        {
+            string s = "Slave";
+
+            var arr = s.ToCharArray();
+
+            var qu = arr.Distinct();
+
+            foreach (var z in qu)
+            {
+                Console.WriteLine("{0} повторяется {1} раз", z, s.Count(c => c == z));
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+}
